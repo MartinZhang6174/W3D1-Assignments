5ce34f73250e850b6a18434d2d74c174bd0ab437
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-11 17:10:25 -0700

    Done part one

--- ScrollView Image Galleries/ScrollView Image Galleries/ViewController.m
+++ ScrollView Image Galleries/ScrollView Image Galleries/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic) BOOL translatesAutoresizingMaskIntoConstraints;
+@property CGSize contentSize;
 
 @end
 
@@ -17,6 +21,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.translatesAutoresizingMaskIntoConstraints = YES;
+    self.contentSize = CGSizeMake(CGRectGetHeight(self.scrollView.frame), CGRectGetWidth(self.scrollView.frame));
+    
+    double firstImageViewX = 0;
+    double secondImageViewX = CGRectGetWidth(self.scrollView.frame);
+    double thirdImageViewX = CGRectGetWidth(self.scrollView.frame) * 2;
+    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(firstImageViewX, 0, CGRectGetHeight(self.scrollView.frame), CGRectGetWidth(self.scrollView.frame))];
+    UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(secondImageViewX, 0, CGRectGetHeight(self.scrollView.frame), CGRectGetWidth(self.scrollView.frame))];
+    UIImageView *imageView3 = [[UIImageView alloc] initWithFrame:CGRectMake(thirdImageViewX, 0, CGRectGetHeight(self.scrollView.frame), CGRectGetWidth(self.scrollView.frame))];
+    imageView1.backgroundColor = [UIColor redColor];
+    imageView2.backgroundColor = [UIColor blueColor];
+    imageView3.backgroundColor = [UIColor orangeColor];
+    
+    [self.scrollView addSubview:imageView1];
+    [self.scrollView addSubview:imageView2];
+    [self.scrollView addSubview:imageView3];
+    
 }
 
 - (void)didReceiveMemoryWarning {
